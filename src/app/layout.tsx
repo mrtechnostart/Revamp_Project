653dc23bad3291d6f9403b7e48e6e54fe7e29775
Ram Badan Pandey <rambpandey238@gmail.com>
2023-09-17 19:25:36 +0530

    Do relative path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import AuthProvider from './Components/AuthProvider'
+import AuthProvider from '@/app/Components/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
