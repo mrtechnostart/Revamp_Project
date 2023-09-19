1aaaf4c322b7df8ebd50c0c9d4bd0d1c453d3f90
mrtechnostart <ram@shivamitech.com>
2023-09-20 00:54:30 +0530

    Add Update Profile UI

--- src/app/(private)/dashboard/profile/page.tsx
+++ src/app/(private)/dashboard/profile/page.tsx
@@ -1,28 +1,92 @@
 "use client"
-import { useSession } from "next-auth/react"
-import { useEffect, useState } from "react"
-import axios from "axios"
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import React, { useEffect, useState } from 'react';
 
+export default function Page() {
+    const [user,setUser] = useState({})
+  const [formData, setFormData] = useState({
+    name: user.name || '',
+    phone: user.phone || '',
+    address: user.address || '',
+  });
 
-export default function Page(){
-    const {data:session,status} = useSession()
-    const [data,setData] = useState(null)
-    
-    useEffect(()=>{
-        let userdata = session?.user?.email
-        async function getData(){
-        const userData = await axios.post("/api/data",{
-            email:userdata
-        })   
-        setData(userData)
-        }
-        getData()
-    },[session])
-    if(data)
-    console.log(data.data.user.email)
-    return(
-        <>
-        Hii From Profile Page
-        </>
-    )
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const {data:session} = useSession()
+  useEffect(()=>{
+    const userData = session?.user?.email
+    if (session){
+        (async()=>{
+            const data = await axios.post('/api/data',{
+                email:userData
+            })
+            setFormData(data)
+        })()
+    }
+  },[session])
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform an API call to update the user's profile here
+    console.log('Updated Profile:', formData);
+    // Reset the form or handle success/failure as needed
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-lg">
+      <h2 className="text-2xl font-semibold mb-4">Update Your Profile</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-gray-600">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="phone" className="block text-gray-600">Phone</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            placeholder="Enter your phone number"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="address" className="block text-gray-600">Address</label>
+          <textarea
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+            placeholder="Enter your address"
+            rows="4"
+          ></textarea>
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600"
+          >
+            Update Profile
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
+
