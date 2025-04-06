9c8849451b8550f75491cb5fb850aaba5ad1a8ef
mrtechnostart <rambpandey238@gmail.com>
2025-04-06 17:18:21 +0530

    Update AAVARTAK Branding|

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center min-h-screen bg-green-100">
       <div className="flex flex-col items-center justify-center space-y-8">
-        <img alt="Aavartak" src="/images/revamp2.png" />
+        <Image height={300} width={300} alt="Aavartak" src="/images/revamp2.png" />
         <Link href="/auth/login" className="bg-green-500 hover:bg-green-700 text-white font-medium py-3 px-6 rounded-full transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">
           Login Now!
         </Link>
